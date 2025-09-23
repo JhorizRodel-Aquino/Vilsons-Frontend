721efbe680a646c0344d1e17ff6a1d68fbb884e5
Jhoriz Rodel Aquino <jhoriz.aquino@gmail.com>
2025-09-23 18:18:45 +0800

    Removed the p element inside the option element in the status filter component.

--- vilsons-frontend/src/components/StatusFilter.tsx
+++ vilsons-frontend/src/components/StatusFilter.tsx
@@ -4,7 +4,7 @@ import Dropdown from "./Dropdown";
 function StatusFilter() {
   return (
     <Dropdown>
-      <option key='all' value="all" className="p-0 m-0"><p>All Status</p></option>
+      <option key='all' value="all" className="p-0 m-0">All Status</option>
         {Object.entries(statusItems).map(([stat, item]) => (
             <option key={stat} value={stat}>
             {item.label}
